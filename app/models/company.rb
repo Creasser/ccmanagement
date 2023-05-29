f1cf9cbc7d32ac70fd1b3f997ca76676da8bfa02
Blake Creasser <blakecreasser@aggienetwork.com>
2023-05-29 12:54:00 -0500

    start working on company model validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ApplicationRecord
+    has_secure_password
+    validates :username, uniqueness: true, presence: true, length: {in: 8..20}
+    validates :company_name, uniqueness: true, presence: true
+    validates :description, length: {maximum: 500}
 end
