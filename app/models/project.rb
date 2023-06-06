5391047b66e020f6a04dcb5e624167c5e273ca6b
Blake Creasser <blakecreasser@aggienetwork.com>
2023-06-05 20:19:44 -0500

    add validations to projects table

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ApplicationRecord
     belongs_to :company
     belongs_to :contractor
+    validates :project_title, length: {in: 1..50}, presence: true
+    validates :price, presence: true
+    validates :start_date, presence: true
+    validates :deadline, presence: true
+    validates :description, presence: true
 end
