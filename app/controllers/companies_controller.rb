ad62adb1cd7f58c8cf79c751b8ed5586d9fd2112
Blake Creasser <blakecreasser@aggienetwork.com>
2023-07-13 19:09:45 -0500

    remove blank space

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,10 +1,2 @@
 class CompaniesController < ApplicationController
-    
-   # def show
-   #     company = Company.find_by(id: params[:id])
-   #     if company
-   #         render :json company, status:
-   #     end
-   # end
-
 end
